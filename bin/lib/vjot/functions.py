818b67dd23647635fe52433ceba1cd23d8326294
Bryan Hann <nobody@nowhere>
2023-05-24 18:23:28 -0230

    Add  '--time' option to [bh.vjot select].'

--- bin/lib/vjot/functions.py
+++ bin/lib/vjot/functions.py
@@ -5,6 +5,15 @@ from pathlib import Path
 from collections import defaultdict
 from vjot.constants import ROOT
 
+def length4vjot(vjot):
+    import mutagen
+    from mutagen.mp3 import MP3
+    try:
+        audio = MP3(vjot)
+        return max( int(audio.info.length), 1 )
+    except mutagen.mp3.HeaderNotFoundError:
+        return 0
+
 def select(prefix):
     fn = lambda vjot : stamp4vjot(vjot).startswith(prefix)
     return sorted( list( filter( fn, vjots() ) ) )
@@ -37,3 +46,9 @@ def vjot4stamp(stamp):
 
 def audio4vjot(vjot):
     os.system( f"bh.beep-play {vjot}")
+
+def audio4stamp(stamp):
+    vjot = vjot4stamp(stamp)
+    if vjot:
+        audio4vjot(vjot)
+        return True
